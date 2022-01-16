6754a10b77f2cae61fb80561c627409504b145df
zmr <mr4z@foxmail.com>
2022-01-16 14:53:32 +0800

    test of import lib

--- test.py
+++ test.py
@@ -1,5 +1,42 @@
-this is a test
-but this is something new from this 
-so this is the newest coding put in
-would like to try some od this?
-of course
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+    
+plt.plot([1, 2, 3]) 
+plt.title('matplotlib.pyplot.plot() example 1') 
+plt.draw() 
+plt.show() 
+
+
+
+# Fixing random state for reproducibility 
+np.random.seed(19680801) 
+    
+# create random data 
+xdata = np.random.random([2, 10]) 
+    
+# split the data into two parts 
+xdata1 = xdata[0, :] 
+xdata2 = xdata[1, :] 
+    
+# sort the data so it makes clean curves 
+xdata1.sort() 
+xdata2.sort() 
+    
+# create some y data points 
+ydata1 = xdata1 ** 2
+ydata2 = 1 - xdata2 ** 3
+    
+# plot the data 
+plt.plot(xdata1, ydata1, color ='tab:blue') 
+plt.plot(xdata2, ydata2, color ='tab:orange') 
+    
+    
+# set the limits 
+plt.xlim([0, 1]) 
+plt.ylim([0, 1]) 
+  
+plt.title('matplotlib.pyplot.plot() example 2') 
+    
+# display the plot 
+plt.show()
